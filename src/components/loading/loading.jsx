9c883e0c31c75d73ba1ce99ef1eb29d36e013607
davidadeola <davidolorunsesan15@gmail.com>
2022-09-29 02:36:24 +0100

    chore: set loading state, and scroll design

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./loading.css";
 
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="spinner">
+      <p>loading...</p>
+      <div className="spinner-area spinner-first"></div>
+      <div className="spinner-area spinner-second"></div>
+      <div className="spinner-area spinner-third"></div>
+    </div>
+  );
 };
 
 export default Loading;
